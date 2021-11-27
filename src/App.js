905a4a6a0d89b3d7b39c788f2631db5f5f277ab0
T.Oka <chita123daa@gmail.com>
2021-11-27 22:09:41 +0900

    タスク追加、残タスク数

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
-
-function addTask(name) {
-  alert(name);
-}
+import { nanoid } from "nanoid";
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const taskList = tasks.map(task => (
     <Todo
-        id={task.id}
-        name={task.name}
-        completed={task.completed}
-        key={task.id}
-      />
-    )
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  )
   );
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+
   return (
     <div className="todoapp stack-large">
       <Form addTask={addTask} />
@@ -25,7 +33,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
