5631b2123ae738d989031e12e57de1ad52fd7a92
T.Oka <chita123daa@gmail.com>
2021-11-28 15:32:07 +0900

    focus管理

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState('');
 
   const editFieldRef = useRef(null);
   const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -85,13 +95,13 @@ function Todo(props) {
   );
 
   useEffect(() => {
-    if (isEditing) {
+    if (!wasEditing && isEditing) {
       editFieldRef.current.focus();
-    } else {
+    }
+    if (wasEditing && !isEditing) {
       editButtonRef.current.focus();
     }
-  }, [isEditing]);
-
+  }, [wasEditing, isEditing]);
 
   return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
