75c51f6a17266d80aa2dbb03ceb3e3164805038a
T.Oka <chita123daa@gmail.com>
2021-12-05 16:21:51 +0900

    Updates

---
+++ precache-manifest.f5e7488ae54094417ce95665527b7680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acef7585656f469e7fdd2c22e162c46e",
+    "url": "/cta-todo-react/index.html"
+  },
+  {
+    "revision": "b978e96dd6bd8f491ba1",
+    "url": "/cta-todo-react/static/css/main.c63a585d.chunk.css"
+  },
+  {
+    "revision": "a8e93749e1134c2f5830",
+    "url": "/cta-todo-react/static/js/2.1146298c.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/cta-todo-react/static/js/2.1146298c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b978e96dd6bd8f491ba1",
+    "url": "/cta-todo-react/static/js/main.a8d05ae2.chunk.js"
+  },
+  {
+    "revision": "b2d7a4026171be146d87",
+    "url": "/cta-todo-react/static/js/runtime-main.1e7736e5.js"
+  }
+]);
